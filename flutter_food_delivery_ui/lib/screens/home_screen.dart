5ba81fd9c46e3baf7b1a17b95f0dac36873186eb
Code4Blessings <robincw35@gmail.com>
2020-08-12 23:56:40 -0400

    Completed AppBar and created recent_orders widget

--- flutter_food_delivery_ui/lib/screens/home_screen.dart
+++ flutter_food_delivery_ui/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../data/data.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,8 +9,53 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(Icons.account_circle), iconSize: 30.0, onPressed: () {}),
+        title: Text('Food Delivery'),
+        actions: [
+          FlatButton(
+              onPressed: () {}, 
+              child: Text(
+                'Cart (${currentUser.cart.length})',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white
+                ),
+                )
+          )
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide(width: 0.8)
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide(
+                    width: 0.8,
+                    color: Theme.of(context).primaryColor
+                    ),
+                    ),
+                hintText: 'Search Food or Restaurants',
+                prefixIcon: Icon(Icons.search, size: 30),
+                suffixIcon: IconButton(icon: Icon(Icons.clear), onPressed: (){}),
+                
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
