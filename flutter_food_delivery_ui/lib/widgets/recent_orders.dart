247e14a4e8705af75476917bdfbc2c5f850364ad
Code4Blessings <robincw35@gmail.com>
2020-08-14 13:14:21 -0400

    Finished food card and did ALT + up

--- flutter_food_delivery_ui/lib/widgets/recent_orders.dart
+++ flutter_food_delivery_ui/lib/widgets/recent_orders.dart
@@ -17,12 +17,67 @@ class RecentOrders extends StatelessWidget {
       ),
       child: Row(
         children: <Widget>[
-          Image(
-            height: 100.0,
-            width: 100.0,
-            image: AssetImage(order.food.imageUrl),
-            fit: BoxFit.cover,
-          )
+          Row(
+            children: <Widget>[
+              ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image(
+                height: 100.0,
+                width: 100.0,
+                image: AssetImage(order.food.imageUrl),
+                fit: BoxFit.cover,
+              ),
+            ),
+          Container(
+            margin: EdgeInsets.all(12.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  order.food.name,
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(height: 4.0),
+                Text(
+                  order.restaurant.name,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  order.date,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                )
+              ],
+            ),
+          ),
+            ] 
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 20.0),
+            width: 48.0,
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(30.0)
+            ),
+            child: IconButton(
+              icon: Icon(Icons.add), 
+              onPressed: () {},
+              iconSize: 30.0,
+              color: Colors.white,
+              ),
+          ),
         ],
       ),
     );
