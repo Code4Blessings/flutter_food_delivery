cd89903d6720edff50b05bda12c670a6bc3951de
Code4Blessings <robincw35@gmail.com>
2020-08-16 13:01:21 -0400

    Home Screen complete

--- flutter_food_delivery_ui/lib/screens/home_screen.dart
+++ flutter_food_delivery_ui/lib/screens/home_screen.dart
@@ -1,13 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/models/restaurant.dart';
 import 'package:flutter_food_delivery_ui/widgets/widgets.dart';
 import '../data/data.dart';
 
 class HomeScreen extends StatefulWidget {
-  @override
+   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
+  
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  _buildRestaurants() {
+    List<Widget> restaurantList = [];
+    restaurants.forEach((Restaurant restaurant) {
+      restaurantList.add(
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          decoration: BoxDecoration(
+            color: Colors.white, 
+            borderRadius: BorderRadius.circular(15.0),
+            border: Border.all(width: 1.0, color: Colors.grey[200]),
+          ),
+          child: Row(children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+                child: Image(
+                height: 150.0,
+                width: 150.0,
+                image: AssetImage(restaurant.imageUrl),
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(12.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    restaurant.name,
+                    style: TextStyle(
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.bold
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  RatingStars(restaurant.rating),
+                  SizedBox(height: 4.0),
+                  Text(
+                    restaurant.address,
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w600
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 4.0),
+                  Text(
+                    '0.2 miles away',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            )
+          ],
+          ),
+        ),
+      );
+    });
+    return Column(children: restaurantList);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,15 +84,11 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('Food Delivery'),
         actions: [
           FlatButton(
-              onPressed: () {}, 
+              onPressed: () {},
               child: Text(
                 'Cart (${currentUser.cart.length})',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white
-                ),
-                )
-          )
+                style: TextStyle(fontSize: 20.0, color: Colors.white),
+              ))
         ],
       ),
       body: ListView(
@@ -38,23 +101,37 @@ class _HomeScreenState extends State<HomeScreen> {
                 fillColor: Colors.white,
                 filled: true,
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide(width: 0.8)
-                ),
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(width: 0.8)),
                 enabledBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(30),
                   borderSide: BorderSide(
-                    width: 0.8,
-                    color: Theme.of(context).primaryColor
-                    ),
-                    ),
+                      width: 0.8, color: Theme.of(context).primaryColor),
+                ),
                 hintText: 'Search Food or Restaurants',
                 prefixIcon: Icon(Icons.search, size: 30),
-                suffixIcon: IconButton(icon: Icon(Icons.clear), onPressed: (){}),
+                suffixIcon:
+                    IconButton(icon: Icon(Icons.clear), onPressed: () {}),
               ),
             ),
           ),
-          RecentOrders()
+          RecentOrders(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Text(
+                  'Nearby Restaurants',
+                  style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.2),
+                ),
+              ),
+              _buildRestaurants()
+            ],
+          )
         ],
       ),
     );
