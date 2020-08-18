74562ccd178ab4d97ffc90dd74ca7fb7ae40400d
Code4Blessings <robincw35@gmail.com>
2020-08-18 09:21:14 -0400

    Added back & favorite buttons to restaurant screen

--- flutter_food_delivery_ui/lib/screens/restaurant_screen.dart
+++ flutter_food_delivery_ui/lib/screens/restaurant_screen.dart
@@ -18,21 +18,34 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
         children: <Widget>[
           Stack(
             children: <Widget>[
-              Image(
-                height: 220.0,
-                width: MediaQuery.of(context).size.width,
-                image: AssetImage(widget.restaurant.imageUrl),
-                fit: BoxFit.cover,
+              Hero(
+                tag: widget.restaurant.imageUrl,
+                child: Image(
+                  height: 220.0,
+                  width: MediaQuery.of(context).size.width,
+                  image: AssetImage(widget.restaurant.imageUrl),
+                  fit: BoxFit.cover,
+                ),
               ),
-              Row(
-                children: <Widget>[
-                  IconButton(
-                    icon: Icon(Icons.arrow_back_ios),
-                    color: Colors.white,
-                    iconSize: 30.0,
-                    onPressed: () => Navigator.pop(context)
-                  )
-                ],
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 50.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.arrow_back_ios),
+                      color: Colors.white,
+                      iconSize: 30.0,
+                      onPressed: () => Navigator.pop(context)
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.favorite),
+                      color: Theme.of(context).primaryColor,
+                      iconSize: 35.0,
+                      onPressed: () => Navigator.pop(context)
+                    )
+                  ],
+                ),
               )
             ],
           )
